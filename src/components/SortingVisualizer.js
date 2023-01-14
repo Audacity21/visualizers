528453fc4fb36cee918245b716ef80d54cb772d0
= <=>
2023-01-14 09:50:41 +0530

    added bars

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./SVNavbar";
+import SVArray from "./SVArray";
 import "./SortingVisualizer.css";
 
 const SortingVisualizer = () => {
   return (
     <div className="sv__container">
       <Navbar />
+      <SVArray />
     </div>
   );
 };
